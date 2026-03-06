7c9c14e45e9340fa303a7825d58f1582bdc2e8e1
Roxifrancke <70011183+Roxifrancke@users.noreply.github.com>
2026-03-06 20:08:52 +0200

    Update useAuthenticatedFetch.js

--- web/frontend/src/hooks/useAuthenticatedFetch.js
+++ web/frontend/src/hooks/useAuthenticatedFetch.js
@@ -1,14 +1,13 @@
-import { useAppBridge } from "@shopify/app-bridge-react";
-import { authenticatedFetch } from "@shopify/app-bridge-utils";
 import { useCallback } from "react";
 
 export function useAuthenticatedFetch() {
-  const app = useAppBridge();
-  return useCallback(
-    (uri, options = {}) => {
-      const fetchFunction = authenticatedFetch(app);
-      return fetchFunction(uri, options);
-    },
-    [app]
-  );
+  return useCallback((uri, options = {}) => {
+    return fetch(uri, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+    });
+  }, []);
 }
