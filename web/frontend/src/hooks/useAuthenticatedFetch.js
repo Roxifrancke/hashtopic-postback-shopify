f87537ae72a884d1ef85f5df0b25b546d08def18
Roxifrancke <70011183+Roxifrancke@users.noreply.github.com>
2026-03-06 22:01:53 +0200

    Update useAuthenticatedFetch.js

--- web/frontend/src/hooks/useAuthenticatedFetch.js
+++ web/frontend/src/hooks/useAuthenticatedFetch.js
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 export function useAuthenticatedFetch() {
   return useCallback((uri, options = {}) => {
     const params = new URLSearchParams(window.location.search);
-    const token = params.get("id_token") || params.get("session") || "";
+    const token = params.get("id_token") || "";
     const shop = params.get("shop") || "";
 
     return fetch(uri, {
